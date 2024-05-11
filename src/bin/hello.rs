b6e2240b75500b8ea040a26bd0812636c3a69a10
sd_ltt <sd_ltt@qq.com>
2024-05-11 19:41:02 +0800

    勉强能用

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,59 +1,262 @@
-#![deny(warnings)]
-
-use std::convert::Infallible;
+use std::error::Error;
 use std::net::SocketAddr;
-
+use std::pin::Pin;
+use std::sync::Mutex;
+ 
+use anyhow::{anyhow, Result};
 use bytes::Bytes;
-use http_body_util::Full;
+use futures::FutureExt;
+use http_body_util::{BodyExt, Empty, Full};
+use http_body_util::combinators::BoxBody;
+use hyper::{header, Request, Response, StatusCode};
+use hyper::body::{Body, Incoming};
+use hyper::client::conn::http1::{Builder, Connection, SendRequest};
+use hyper::rt::{Read, Write};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
-use tokio::net::TcpListener;
-
-
-// An async function that consumes a request, does nothing with it and returns a
-// response.
-async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
-}
-
+use hyper::upgrade::Upgraded;
+use hyper_util::rt::TokioIo;
+use openssl::ssl::{Ssl, SslAcceptor, SslConnector, SslMethod, SslVerifyMode};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_openssl::SslStream;
+ 
+pub mod crt;
+ 
+type HttpBody = BoxBody<Bytes, hyper::Error>;
+ 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    pretty_env_logger::init();
-
+pub async fn main() -> Result<()> {
     // This address is localhost
-    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
-
+    let addr: SocketAddr = "127.0.0.1:7890".parse().unwrap();
+ 
     // Bind to the port and listen for incoming TCP connections
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
     loop {
-        // When an incoming TCP connection is received grab a TCP stream for
-        // client<->server communication.
-        //
-        // Note, this is a .await point, this loop will loop forever but is not a busy loop. The
-        // .await point allows the Tokio runtime to pull the task off of the thread until the task
-        // has work to do. In this case, a connection arrives on the port we are listening on and
-        // the task is woken up, at which point the task is then put back on a thread, and is
-        // driven forward by the runtime, eventually yielding a TCP stream.
-        let (tcp, _) = listener.accept().await?;
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(tcp);
-
-        // Spin up a new task in Tokio so we can continue to listen for new TCP connection on the
-        // current task without waiting for the processing of the HTTP1 connection we just received
-        // to finish
+        let (tcp_stream, addr) = listener.accept().await?;
+        let _msg = format!("{addr} connected");
+        //dbg!(msg);
         tokio::task::spawn(async move {
-            // Handle the connection from the client using HTTP1 and pass any
-            // HTTP requests received on that connection to the `hello` function
-            if let Err(err) = http1::Builder::new()
-                .timer(TokioTimer::new())
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
+            let io = TokioIo::new(tcp_stream);
+ 
+            let conn = http1::Builder::new()
+                .serve_connection(io, service_fn(handle));
+ 
+            // Don't forget to enable upgrades on the connection.
+            let mut conn = conn.with_upgrades();
+ 
+            let conn = Pin::new(&mut conn);
+            if let Err(err) = conn.await {
                 println!("Error serving connection: {:?}", err);
             }
         });
     }
 }
+ 
+fn get_host_port(host_name: &str) -> (&str, u16) {
+    match host_name.find(":") {
+        None => {
+            (host_name, 80)
+        }
+        Some(i) => {
+            (&host_name[0..i], *&host_name[i + 1..].parse().unwrap_or(80))
+        }
+    }
+}
+ 
+fn not_found_host() -> Response<HttpBody> {
+    Response::builder().status(404).body(full("not found host")).unwrap()
+}
+ 
+/// Our server HTTP handler to initiate HTTP upgrades.
+async fn handle(mut req: Request<Incoming>) -> Result<Response<HttpBody>> {
+    if req.method() != hyper::Method::CONNECT {
+        let (host, port) = match req.headers().get(header::HOST) {
+            None => {
+                return Ok(not_found_host());
+            }
+            Some(h) => { get_host_port(h.to_str()?) }
+        };
+ 
+        let stream = TcpStream::connect((host, port)).await?;
+        let io = TokioIo::new(stream);
+ 
+        let (mut sender, conn) = Builder::new()
+            .preserve_header_case(true)
+            .title_case_headers(true)
+            .handshake(io)
+            .await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+        let (req, resp) = intercept_request(req);
+        return match resp {
+            None => {
+                let resp = sender.send_request(req).await?;
+                Ok(resp.map(|b| b.boxed()))
+            }
+            Some(resp) => {
+                Ok(resp)
+            }
+        };
+    }
+ 
+    let res = Response::new(empty());
+    // handle https
+    tokio::task::spawn(async move {
+        match hyper::upgrade::on(&mut req).await {
+            Ok(upgraded) => {
+                if let Some(host) = req.uri().host() {
+                    if let Err(e) = server_upgraded_https(host, upgraded).await {
+                        let error_msg = format!("server io error: {}", e);
+                        dbg!(error_msg);
+                    };
+                }
+            }
+            Err(e) => eprintln!("upgrade error: {}", e),
+        }
+    });
+ 
+    Ok(res)
+}
+ 
+ 
+/// https upgraded
+async fn server_upgraded_https(host: &str, upgraded: Upgraded) -> Result<()> {
+    let upgraded = TokioIo::new(upgraded);
+    // we have an upgraded connection that we can read and
+    // write on directly.
+    //
+    let tls_acceptor = get_tls_acceptor(host);
+    let ssl = Ssl::new(tls_acceptor.context())?;
+    let mut tls_stream = SslStream::new(ssl, upgraded)?;
+    if let Err(err) = SslStream::accept(Pin::new(&mut tls_stream)).await {
+        return Err(anyhow!("error during tls handshake connection from : {}", err));
+    }
+    let stream = TokioIo::new(tls_stream);
+ 
+    let (sender, conn) = https_remote_connect(host, 443).await?;
+    tokio::spawn(async move {
+        if let Err(err) = conn.await {
+            let err_msg = format!("Connection failed: {:?}", err);
+            dbg!(err_msg);
+        }
+    });
+    let wrap_sender = Mutex::new(sender);
+    if let Err(err) = http1::Builder::new()
+        .serve_connection(stream, service_fn(|req| {
+            let (req, resp) = intercept_request(req);
+            async {
+                match resp {
+                    None => {
+                        let remote_resp = wrap_sender.lock().unwrap().send_request(req);
+                        match remote_resp.await {
+                            Ok(resp) => {
+                                Ok::<_, hyper::Error>(intercept_response(resp))
+                            }
+                            Err(err) => {
+                                let resp = Response::builder()
+                                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                    .header(header::CONTENT_TYPE, "text/plain")
+                                    .body(full(err.to_string())).unwrap();
+                                Ok::<_, hyper::Error>(resp)
+                            }
+                        }
+                    }
+                    Some(resp) => {
+                        Ok::<_, hyper::Error>(resp)
+                    }
+                }
+            }
+        })).await {
+        println!("Error serving connection: {:?}", err);
+    }
+ 
+    Ok(())
+}
+ 
+ 
+fn empty() -> HttpBody {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+ 
+fn full<T: Into<Bytes>>(chunk: T) -> HttpBody {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+ 
+/// Certificate not cached
+fn get_tls_acceptor(host: &str) -> SslAcceptor {
+    let mut tls_builder = SslAcceptor::mozilla_modern_v5(SslMethod::tls()).unwrap();
+ 
+    let (crt, pri_key) = crt::get_crt_key(host);
+ 
+    tls_builder.set_certificate(&crt).unwrap();
+ 
+    tls_builder.set_private_key(&pri_key).unwrap();
+ 
+    tls_builder.check_private_key().unwrap();
+ 
+    let tls_acceptor = tls_builder.build();
+    tls_acceptor
+}
+ 
+ 
+async fn https_remote_connect<B>(host: &str, port: u16) -> Result<(SendRequest<B>, Connection<TokioIo<SslStream<TcpStream>>, B>)>
+    where
+        B: Body + 'static,
+        B::Data: Send,
+        B::Error: Into<Box<dyn Error + Send + Sync>>, {
+    let addr = format!("{}:{}", host, port);
+    let tcp_stream = TcpStream::connect(addr).await?;
+    let mut builder = SslConnector::builder(SslMethod::tls_client())?;
+    builder.set_verify(SslVerifyMode::NONE);
+    let connector = builder.build();
+    let ssl = Ssl::new(connector.context())?;
+    let mut tls_stream = SslStream::new(ssl, tcp_stream)?;
+    if let Err(err) = SslStream::connect(Pin::new(&mut tls_stream)).await {
+        return Err(anyhow!("error during tls handshake connection from : {}", err));
+    }
+    let io = TokioIo::new(tls_stream);
+    Ok(hyper::client::conn::http1::handshake(io).await?)
+}
+ 
+/// Intercept local requests
+fn intercept_request(mut request: Request<Incoming>) -> (Request<HttpBody>, Option<Response<HttpBody>>) {
+    dbg!(request.uri().to_string());
+    request.headers_mut().remove("Accept-Encoding");
+    let req = request.map(|b| b.boxed());
+ 
+    if let Some(Ok(host)) = req.headers().get(header::HOST).map(|h| h.to_str()) {
+        if host.contains("127.0.0.1:7890")
+            || host.contains("localhost:7890")
+            || host.contains("baidu") {
+            let resp = Response::builder()
+                .header(header::CONTENT_TYPE, "text/plain")
+                .body(full("Proxylea Server Power By Rust & Hyper\n"));
+            return (req, Some(resp.unwrap()));
+        }
+    }
+    (req, None)
+}
+ 
+/// Intercept remote responses
+fn intercept_response(mut response: Response<Incoming>) -> Response<HttpBody> {
+    dbg!({ format!("{:?}", response.headers()) });
+    response.headers_mut().insert("proxy-server", "Proxylea".parse().unwrap());
+    //let (parts,incoming)=resp.into_parts();
+    let resp = response.map(|b| b.map_frame(|frame| {
+        if let Some(bytes) = frame.data_ref() {
+            //
+        }
+        frame
+    }).boxed());
+    resp
+}
+ 
+ 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,13 +1,20 @@
-use std::pin::Pin;
-use openssl::ssl::{SslContext, SslFiletype, SslMethod, Ssl};
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use openssl::ssl::{Ssl, SslAcceptor, SslContext, SslFiletype, SslMethod};
 use tokio_openssl::SslStream;
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-use bytes::Bytes;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::upgrade;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
 // async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Bytes>> {
 //     Ok(Response::new(Bytes::from("Hello World!")))
@@ -16,56 +23,41 @@ use hyper::{Request, Response};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 加载 SSL 证书和私钥
-	let mut ssl_ctx = SslContext::builder(SslMethod::tls()).unwrap();
-	ssl_ctx.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
-	ssl_ctx.set_certificate_file("cert.pem", SslFiletype::PEM).unwrap();
-	ssl_ctx.check_private_key().unwrap();
-	let ssl_ctx = ssl_ctx.build();
-	// let ssl = Ssl::new(ssl_ctx.as_ref()).unwrap();
+    let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    acceptor
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    acceptor.set_certificate_chain_file("cert.pem").unwrap();
+    acceptor.check_private_key().unwrap();
+    let acceptor = Arc::new(acceptor.build());
+    // let ssl = Ssl::new(ssl_ctx.as_ref()).unwrap();
 
     let listener = TcpListener::bind("127.0.0.1:8443").await?;
-	
-	loop {
-		match listener.accept().await {
-			Ok((stream, _)) => {
-				
-				let ssl = Ssl::new(ssl_ctx.as_ref()).unwrap();
-				tokio::spawn(async move {
-					let mut stream = std::pin::pin!(SslStream::new(ssl, stream).unwrap());
-					stream.as_mut().accept().await.unwrap();
-					match stream.as_mut().do_handshake().await {
-						Ok(_) => {
-							// if let Err(err) = http1::Builder::new()
-							// 	.serve_connection(stream, service_fn(hello))
-							// 	.await
-							// {
-							// 	println!("Error serving connection: {:?}", err);
-							// }
-							if let Err(err) = process_connection(stream.as_mut()).await {
-								eprintln!("Error processing connection: {}", err);
-							}
-						},
-						Err(err) => {
-						    eprintln!("Error do_handshake: {}", err);
-						}
-					}
-					
-				});
-			},
-			Err(err) => {
-			    eprintln!("Error accepting connection: {}", err)
-			}
-		};
-		
-	}
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                let acceptor = acceptor.clone();
+                tokio::spawn(async move {
+                    let ssl = Ssl::new(acceptor.context()).unwrap();
+                    let mut tls_stream = SslStream::new(ssl, stream).unwrap();
+                    SslStream::accept(Pin::new(&mut tls_stream)).await.unwrap();
+                    // let stream = TokioIo::new(stream);
+                    let stream = TokioIo::new(tls_stream);
+                    let conn = http1::Builder::new().serve_connection(stream, service_fn(handle));
+                    if let Err(err) = conn.await {
+                        eprintln!("Error serving connection: {}", err);
+                    }
+                });
+            }
+            Err(err) => {
+                eprintln!("Error accepting connection: {}", err)
+            }
+        };
+    }
 }
 
-async fn process_connection<'a>(mut stream: Pin<&mut tokio_openssl::SslStream<tokio::net::TcpStream>>) -> Result<(), Box<dyn std::error::Error>> {
-        
-	let response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello World!\n";
-
-	stream.write_all(response.as_bytes()).await?;
-	stream.flush().await?;
-	stream.shutdown().await.unwrap();
-	Ok(())
+async fn handle(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
+
