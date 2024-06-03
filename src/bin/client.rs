fb158d2999b3a1d8b71d5b36b5fad32f17fc43f0
sd_ltt <sd_ltt@qq.com>
2024-06-03 17:30:07 +0800

    增加代码注释

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -10,38 +10,56 @@ use hyper_tls::HttpsConnector;
 use tokio::net::TcpStream;
 
 #[allow(unused)]
+// 异步函数，用于获取URL
 async fn get(
     url: &hyper::Uri,
 ) -> Result<Response<Incoming>, Box<dyn std::error::Error + Send + Sync>> {
+    // 从URL中获取主机名
     let host = url.host().expect("[GET] url has no host");
+    // 从URL中获取端口号，如果没有则默认为80
     let port = url.port_u16().unwrap_or(80);
+    // 拼接主机名和端口号
     let address = format!("{}:{}", host, port);
+    // 建立与主机的TCP连接
     let stream = TcpStream::connect(address).await?;
+    // 将TCP流转换为TokioIo
     let io = TokioIo::new(stream);
+    // 从URL中获取作者名称
     let authority = url.authority().unwrap().clone();
+    // 构建客户端
     let client = Client::builder(TokioExecutor::new()).build::<_,Empty<Bytes>>(HttpsConnector::new());
+    // 发起GET请求
     let res = client.get(url.clone()).await?;
+    // 返回结果
     Ok(res)
 }
 
 #[allow(unused)]
+// 异步函数，用于删除指定url
 async fn delete(
     url: &hyper::Uri,
 ) -> Result<Response<Incoming>, Box<dyn std::error::Error + Send + Sync>> {
+    // 从url中获取主机和端口
     let host = url.host().expect("[DELETE] url has no host");
     let port = url.port_u16().unwrap_or(80);
     let address = format!("{}:{}", host, port);
+    // 使用TcpStream连接主机和端口
     let stream = TcpStream::connect(address).await?;
     let io = TokioIo::new(stream);
+    // 从url中获取 authority
     let authority = url.authority().unwrap().clone();
+    // 进行HTTP1的握手
     let (mut sender, _conn) = hyper::client::conn::http1::handshake(io).await?;
+    // 创建一个DELETE请求
     let req = Request::builder()
         .method("DELETE")
         .uri(url)
         .header(hyper::header::HOST, authority.as_str())
         .body(Empty::<Bytes>::new())?;
 
+    // 发送请求
     let res = sender.send_request(req).await?;
+    // 返回结果
     Ok(res)
 }
 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,8 @@
 use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod};
-use tokio_openssl::SslStream;
 use std::io::{Read, Write};
 use std::pin::Pin;
 use std::sync::Arc;
+use tokio_openssl::SslStream;
 
 use std::convert::Infallible;
 
@@ -25,39 +25,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     acceptor
         .set_private_key_file(format!("{}/ca.key", CERT_PATH), SslFiletype::PEM)
         .unwrap();
-    acceptor.set_certificate_chain_file(format!("{}/ca.crt", CERT_PATH)).unwrap();
+    acceptor
+        .set_certificate_chain_file(format!("{}/ca.crt", CERT_PATH))
+        .unwrap();
     acceptor.check_private_key().unwrap();
     // acceptor.set_min_proto_version(Some(openssl::ssl::SslVersion::TLS1_3)).unwrap();
     let acceptor = Arc::new(acceptor.build());
     // let ssl = Ssl::new(ssl_ctx.as_ref()).unwrap();
 
+    // 监听端口
     let listener = TcpListener::bind("127.0.0.1:8443").await?;
 
     loop {
+        // 处理 TCP 连接
         match listener.accept().await {
             Ok((stream, _)) => {
                 let acceptor = acceptor.clone();
+
+                // 异步 tokio::spawn 函数，用于处理客户端连接
                 tokio::spawn(async move {
+                    // 创建 SSL 上下文
                     let ssl = Ssl::new(acceptor.context()).unwrap();
 
+                    // 创建 SSL 流
                     let mut tls_stream = match SslStream::new(ssl, stream) {
                         Ok(stream) => stream,
                         Err(err) => {
                             eprintln!("Error creating SSL stream: {}", err);
-                            return ;
+                            return;
                         }
                     };
 
+                    // 接受客户端连接
                     if let Err(error) = SslStream::accept(Pin::new(&mut tls_stream)).await {
+                        // 接受连接失败
                         eprintln!("Error accepting connection: {}", error);
-                        return ;
+                        return;
                     }
-                    // let stream = TokioIo::new(stream);
+                    // 创建 TokioIo 流
                     let stream = TokioIo::new(tls_stream);
+                    // 创建 HTTP1 服务
                     let conn = http1::Builder::new().serve_connection(stream, service_fn(handle));
+                    // 处理客户端连接
                     if let Err(err) = conn.await {
+                        // 处理连接失败
                         eprintln!("Error serving connection: {}", err);
-                        return ;
+                        return;
                     }
                 });
             }
@@ -68,11 +81,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-async fn handle(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+// 异步函数，处理请求
+async fn handle(
+    request: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    // 根据请求方法，调用相应的处理函数
     match *request.method() {
         hyper::Method::GET => handle_get(request).await,
         hyper::Method::POST => handle_post(request).await,
         _ => {
+            // 如果请求方法不是GET或POST，返回404错误
             let response = Response::builder()
                 .status(hyper::StatusCode::NOT_FOUND)
                 .body(Full::new(Bytes::from("404 Not Found")))
@@ -82,6 +100,7 @@ async fn handle(request: Request<hyper::body::Incoming>) -> Result<Response<Full
     }
 }
 
+// 读取文件内容
 fn read_file(path: &str) -> Result<String, std::io::Error> {
     let mut file = std::fs::File::open(path)?;
     let mut buf = String::new();
@@ -90,7 +109,9 @@ fn read_file(path: &str) -> Result<String, std::io::Error> {
 }
 
 // 处理 Get 请求, 返回对应路径的文件内容
-async fn handle_get(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn handle_get(
+    request: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
     let path = request.uri().path();
     // 定义一个基本的 CSP 策略字符串, 允许从当前源加载, 并且只允许内联样式
     let csp_header_value = "default-src 'self'; script-src 'self';";
@@ -104,58 +125,64 @@ async fn handle_get(request: Request<hyper::body::Incoming>) -> Result<Response<
             let file_path = format!("{}/XSS/index.html", ROOT_PATH);
             match read_file(file_path.as_str()) {
                 Ok(buf) => {
-                    // 创建响应并添加 CSP 头
+                    // 创建响应
                     let mut response = Response::new(Full::new(Bytes::from(buf)));
+                    // 添加 cookie 头
                     response.headers_mut().insert(header::SET_COOKIE, cookie);
-                    response.headers_mut().insert(header::CONTENT_SECURITY_POLICY, csp_header);
+                    // 添加 CSP 头
+                    response
+                        .headers_mut()
+                        .insert(header::CONTENT_SECURITY_POLICY, csp_header);
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
                 }
             }
-        },
+        }
         "/XSS/unsafe" => {
             let file_path = format!("{}/XSS/index.html", ROOT_PATH);
             match read_file(file_path.as_str()) {
                 Ok(buf) => {
+                    // 生成响应
                     let mut response = Response::new(Full::new(Bytes::from(buf)));
+                    // 添加 cookie 头
                     response.headers_mut().insert(header::SET_COOKIE, cookie);
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
                 }
             }
-        },
+        }
         "/XSS/input" => {
             let file_path = format!("{}/XSS/input.txt", ROOT_PATH);
             match read_file(file_path.as_str()) {
                 Ok(buf) => {
                     let response = Response::new(Full::new(Bytes::from(buf)));
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
                 }
             }
-        },
+        }
         "/XSS/handle.js" => {
             let file_path = format!("{}/{}", ROOT_PATH, path);
             match read_file(file_path.as_str()) {
                 Ok(buf) => {
                     let response = Response::new(Full::new(Bytes::from(buf)));
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
                 }
             }
-        },
+        }
         _ => {
             let response = Response::new(Full::new(Bytes::from("404 Not Found")));
             Ok(response)
@@ -170,34 +197,45 @@ fn write_file(path: &str, content: &str) -> Result<(), std::io::Error> {
 }
 
 // 处理 Post 请求, 将 返回 OK
-async fn handle_post(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn handle_post(
+    request: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
     let path = request.uri().path().to_string();
 
-    let body = request.into_body().frame().await.unwrap().unwrap().into_data().unwrap();
+    let body = request
+        .into_body()
+        .frame()
+        .await
+        .unwrap()
+        .unwrap()
+        .into_data()
+        .unwrap();
     let body = String::from_utf8(body.to_vec()).unwrap();
     match path.as_str() {
         "/XSS/safe" => {
             let file_path = format!("{}/XSS/input.txt", ROOT_PATH);
             // 使用 ammonia 库来清理 HTML 内容
             let body = ammonia::clean(&body);
+            // 将过滤后输入存入文件
             match write_file(&file_path, &body) {
                 Ok(_) => {
                     let response = Response::new(Full::new(Bytes::from("OK")));
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
                 }
             }
-        },
+        }
         "/XSS/unsafe" => {
             let file_path = format!("{}/XSS/input.txt", ROOT_PATH);
+            // 将输入直接存入文件
             match write_file(&file_path, &body) {
                 Ok(_) => {
-                    let response = Response::new(Full::new(Bytes::from("OK"))); 
+                    let response = Response::new(Full::new(Bytes::from("OK")));
                     Ok(response)
-                },
+                }
                 Err(_) => {
                     let response = Response::new(Full::new(Bytes::from("404 Not Found")));
                     Ok(response)
@@ -210,8 +248,3 @@ async fn handle_post(request: Request<hyper::body::Incoming>) -> Result<Response
         }
     }
 }
-
-
-
-
-
